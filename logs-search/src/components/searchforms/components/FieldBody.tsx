76524e4034e4eaef7eef0b28d981a12a2a733c53
mazatf2 <2814748+mazatf2@users.noreply.github.com>
2020-11-18 12:52:29 +0200

    move log-search to https://github.com/mazatf2/logview

--- logs-search/src/components/searchforms/components/FieldBody.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const FieldBody = ({...props}) => {
-	return (
-		<div className="field-body">
-			{props.children}
-		</div>
-	)
-}
--- logs-search/src/loging.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import {Redirect as RouterRedirect} from 'react-router-dom'
-
-export const Redirect = (why: any[]) => {
-	console.log('redirect: ', why)
-	return <RouterRedirect to="/"/>
-}
